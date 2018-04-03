118d3ac869eabf04a35d0a1c26a3a1725af1e8b3
Jun Bon <junbon@binary.com>
2018-04-03 06:36:15 +0000

    moved Vanillas into NonBinaries

--- lib/Math/Business/BlackScholes/NonBinaries.pm
+++ lib/Math/Business/BlackScholes/NonBinaries.pm
@@ -3,7 +3,7 @@ package Math::Business::BlackScholes::NonBinaries;
 use strict;
 use warnings;
 
-use Math::Business::BlackScholes::Vanillas;
+use Math::CDF qw(pnorm);
 
 =head2 callspread
 
@@ -18,8 +18,7 @@ use Math::Business::BlackScholes::Vanillas;
 sub callspread {
     my ($S, $U, $D, $t, $r_q, $mu, $sigma) = @_;
 
-    return Math::Business::BlackScholes::Vanillas::vanilla_call($S, $D, $t, $r_q, $mu, $sigma) -
-        Math::Business::BlackScholes::Vanillas::vanilla_call($S, $U, $t, $r_q, $mu, $sigma);
+    return vanilla_call($S, $D, $t, $r_q, $mu, $sigma) - vanilla_call($S, $U, $t, $r_q, $mu, $sigma);
 }
 
 =head2 putspread
@@ -35,8 +34,45 @@ sub callspread {
 sub putspread {
     my ($S, $U, $D, $t, $r_q, $mu, $sigma) = @_;
 
-    return Math::Business::BlackScholes::Vanillas::vanilla_put($S, $U, $t, $r_q, $mu, $sigma) -
-        Math::Business::BlackScholes::Vanillas::vanilla_put($S, $D, $t, $r_q, $mu, $sigma);
+    return vanilla_put($S, $U, $t, $r_q, $mu, $sigma) - vanilla_put($S, $D, $t, $r_q, $mu, $sigma);
+}
+
+=head2 vanilla_call
+
+    USAGE
+    my $price = vanilla_call($S, $K, $t, $r_q, $mu, $sigma)
+
+    DESCRIPTION
+    Price of a Vanilla Call
+
+=cut
+
+sub vanilla_call {
+    my ($S, $K, $t, $r_q, $mu, $sigma) = @_;
+
+    my $d1 = (log($S / $K) + ($mu + $sigma * $sigma / 2.0) * $t) / ($sigma * sqrt($t));
+    my $d2 = $d1 - ($sigma * sqrt($t));
+
+    return exp(-$r_q * $t) * ($S * exp($mu * $t) * pnorm($d1) - $K * pnorm($d2));
+}
+
+=head2 vanilla_put
+
+    USAGE
+    my $price = vanilla_put($S, $K, $t, $r_q, $mu, sigma)
+
+    DESCRIPTION
+    Price a standard Vanilla Put
+
+=cut
+
+sub vanilla_put {
+    my ($S, $K, $t, $r_q, $mu, $sigma) = @_;
+
+    my $d1 = (log($S / $K) + ($mu + $sigma * $sigma / 2.0) * $t) / ($sigma * sqrt($t));
+    my $d2 = $d1 - ($sigma * sqrt($t));
+
+    return -1 * exp(-$r_q * $t) * ($S * exp($mu * $t) * pnorm(-$d1) - $K * pnorm(-$d2));
 }
 
 1;
