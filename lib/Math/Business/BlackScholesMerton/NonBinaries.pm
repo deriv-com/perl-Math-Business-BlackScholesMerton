5a2d3f69b563619183c0d9a8dfda8032d62447e3
Jun Bon <junbon@binary.com>
2018-05-17 08:48:59 +0000

    callput spread into non binaries

--- lib/Math/Business/BlackScholesMerton/NonBinaries.pm
+++ lib/Math/Business/BlackScholesMerton/NonBinaries.pm
@@ -259,4 +259,36 @@ sub dnorm {    # Standard normal density function
     return $value;
 }
 
+=head2 callspread
+
+    USAGE
+    my $price = callspread($S, $U, $D, $t, $r_q, $mu, $sigmaU, $sigmaD);
+
+    DESCRIPTION
+    Price of a CALL SPREAD
+
+=cut
+
+sub callspread {
+    my ($S, $U, $D, $t, $r_q, $mu, $sigmaU, $sigmaD) = @_;
+
+    return vanilla_call($S, $D, $t, $r_q, $mu, $sigmaD) - vanilla_call($S, $U, $t, $r_q, $mu, $sigmaU);
+}
+
+=head2 putspread
+
+    USAGE
+    my $price = putspread($S, $U, $D, $t, $r_q, $mu, $sigmaU, $sigmaD);
+
+    DESCRIPTION
+    Price of a PUT SPREAD
+
+=cut
+
+sub putspread {
+    my ($S, $U, $D, $t, $r_q, $mu, $sigmaU, $sigmaD) = @_;
+
+    return vanilla_put($S, $U, $t, $r_q, $mu, $sigmaU) - vanilla_put($S, $D, $t, $r_q, $mu, $sigmaD);
+}
+
 1;
